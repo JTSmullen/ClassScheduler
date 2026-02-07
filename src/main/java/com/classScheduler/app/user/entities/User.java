24eacd36da6dd8526441f2558c4b5fac4a4d0079
Carl Foerster <cjfoerster5@gmail.com>
2026-02-06 22:24:08 -0500

    user class stub

---
+++ src/main/java/com/classScheduler/app/user/entities/User.java
@@ -0,0 +1,30 @@
+package com.classScheduler.app.user.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+    private Advisor advisor;
+    private ArrayList<CandidateSchedule> candidateScheduleList;
+    private ArrayList<SavedSchedule> savedScheduleList;
+    private Search search;
+    private String username;
+    private String password;
+
+
+    public void addCandidateSchedule(CandidateSchedule s) {
+        this.candidateScheduleList.add(s);
+    }
+
+    public void addSavedSchedule(SavedSchedule s) {
+        this.savedScheduleList.add(s);
+    }
+
+
+
+
+
+
+}
