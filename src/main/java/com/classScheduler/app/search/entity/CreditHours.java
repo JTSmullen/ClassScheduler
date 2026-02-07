e480062ea207fc98a2a09b381fd6cbe36ff6c4b2
VIEKMANCJ22 <cjviekman@gmail.com>
2026-02-06 22:56:00 -0500

    Added stubs for Search and Filter classes

---
+++ src/main/java/com/classScheduler/app/search/entity/CreditHours.java
@@ -0,0 +1,12 @@
+package com.classScheduler.app.search.entity;
+public enum CreditHours {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4);
+
+    public final int value;
+    private CreditHours(int value) {
+        this.value = value;
+    }
+}
