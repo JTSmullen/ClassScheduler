632fb8a1de8213ce732aa2612814a7bb4f218b93
Carl Foerster <cjfoerster5@gmail.com>
2026-02-24 11:06:37 -0500

    Made Course Removal database friendly

--- ClassBackend/src/main/java/com/classScheduler/app/schedule/service/ScheduleService.java
+++ ClassBackend/src/main/java/com/classScheduler/app/schedule/service/ScheduleService.java
@@ -2,14 +2,27 @@ package com.classScheduler.app.schedule.service;
 
 import com.classScheduler.app.course.entity.CourseSection;
 import com.classScheduler.app.schedule.entity.Schedule;
+import com.classScheduler.app.schedule.repository.ScheduleRepository;
 import com.classScheduler.app.user.entities.User;
 
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+
+import com.classScheduler.app.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
 @Service
 public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepo;
+
+    public ScheduleService(ScheduleRepository scheduleRepo) {
+        this.scheduleRepo = scheduleRepo;
+    }
 
     public CourseSection addCourse(Schedule schedule, CourseSection section) {
         /*
@@ -21,7 +34,7 @@ public class ScheduleService {
     public CourseSection removeCourse(Schedule schedule, CourseSection section) {
         List<CourseSection> courses = schedule.getCourses();
         courses.remove(section);
-        schedule.setCourses(courses);
+        scheduleRepo.save(schedule);
         return null;
     }
 
