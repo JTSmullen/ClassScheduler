8108df81291e7f393f957026245fb85387925829
Smullen <smullenjt23@gcc.edu>
2026-02-25 17:19:30 -0500

    Bug Fix for H2 Connection

--- ClassBackend/src/main/java/com/classScheduler/app/schedule/repository/ScheduleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.classScheduler.app.schedule.repository;
-
-import com.classScheduler.app.schedule.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository <Schedule, Long> { }
--- ClassBackend/src/main/java/com/classScheduler/app/user/repository/UserRepository.java
+++ ClassBackend/src/main/java/com/classScheduler/app/user/repository/UserRepository.java
@@ -3,13 +3,13 @@ package com.classScheduler.app.user.repository;
 import com.classScheduler.app.user.entities.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-    Optional<User> findByName(String username);
+    Optional<User> findByName(String name);
 
 }
