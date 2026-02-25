64387a5c163df24c356ebbd7c456676f7da996e6
VIEKMANCJ22 <cjviekman@gmail.com>
2026-02-25 07:58:56 -0500

    Began adding Search class

---
+++ ClassBackend/src/main/java/com/classScheduler/app/search/entity/Search.java
@@ -0,0 +1,7 @@
+package com.classScheduler.app.search.entity;
+
+import com.classScheduler.app.filter.enums.CreditHours;
+import com.classScheduler.app.filter.enums.Department;
+import com.classScheduler.app.filter.enums.CourseCode;
+import com.classScheduler.app.filter.enums.Professor;
+
