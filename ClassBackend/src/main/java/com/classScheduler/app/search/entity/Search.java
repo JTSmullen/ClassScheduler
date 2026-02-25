8e8d435d451d964cd8dbb029cccc0f5f68cd3c2b
VIEKMANCJ22 <cjviekman@gmail.com>
2026-02-25 13:55:41 -0500

    updated search

--- ClassBackend/src/main/java/com/classScheduler/app/search/entity/Search.java
+++ ClassBackend/src/main/java/com/classScheduler/app/search/entity/Search.java
@@ -4,4 +4,17 @@ import com.classScheduler.app.filter.enums.CreditHours;
 import com.classScheduler.app.filter.enums.Department;
 import com.classScheduler.app.filter.enums.CourseCode;
 import com.classScheduler.app.filter.enums.Professor;
+import com.classScheduler.app.course.entity.Course;
 
+public class Search {
+    protected Course results;
+    protected Course filteredResults;
+
+    public Search() {
+        this.results = results;
+        // no filters applied yet
+        this.filteredResults = new ArrayList<>(results);
+    }
+
+
+}
