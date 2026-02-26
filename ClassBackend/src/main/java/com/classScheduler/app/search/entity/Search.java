1d5bacb2c10afb4a6de68102b1c896779f81526a
VIEKMANCJ22 <cjviekman@gmail.com>
2026-02-26 12:27:11 -0500

    Updated search and SearchService

--- ClassBackend/src/main/java/com/classScheduler/app/search/entity/Search.java
+++ ClassBackend/src/main/java/com/classScheduler/app/search/entity/Search.java
@@ -4,15 +4,16 @@ import com.classScheduler.app.filter.enums.CreditHours;
 import com.classScheduler.app.filter.enums.Department;
 import com.classScheduler.app.filter.enums.CourseCode;
 import com.classScheduler.app.filter.enums.Professor;
-import com.classScheduler.app.course.entity.Course;
+import com.classScheduler.app.course.entity.CourseSection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Search {
-    protected Course results;
-    protected Course filteredResults;
+    private ArrayList<CourseSection> results;
+    private ArrayList<CourseSection> filteredResults;
 
-    public Search(ArrayList<Course> results) {
+    public Search(ArrayList<CourseSection> results) {
         this.results = results;
         // no filters applied yet
         this.filteredResults = new ArrayList<>(results);
---
+++ ClassBackend/src/main/java/com/classScheduler/app/search/service/SearchService.java
@@ -0,0 +1,15 @@
+package com.classScheduler.app.search.service;
+
+import com.classScheduler.app.filter.enums.CreditHours;
+import com.classScheduler.app.filter.enums.Department;
+import com.classScheduler.app.filter.enums.CourseCode;
+import com.classScheduler.app.filter.enums.Professor;
+import com.classScheduler.app.course.entity.CourseSection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SearchService {
+
+}
