b388f7d0541e0435069d5fc9b4547c755554e2fb
Smullen <smullenjt23@gcc.edu>
2026-04-17 12:52:49 -0400

    [Add] -> Register Page

--- ClassFrontend/src/app/app.routes.ts
+++ ClassFrontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { LoginPage } from './login-page/login-page';
+import { RegisterPage } from './register-page/register-page';
 
 export const routes: Routes = [
 
     { path: '', component: LandingPage },
     { path: 'login', component: LoginPage },
+    { path: 'register', component: RegisterPage },
     { path: '**', redirectTo: '' }
 
 ];
