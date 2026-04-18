edb839392cca464f73ab11d3e1a7bcda1f6ac2f9
Carl Foerster <cjfoerster5@gmail.com>
2026-04-18 18:58:10 -0400

    .

--- ClassFrontend/src/app/app.routes.ts
+++ ClassFrontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { LoginPage } from './login-page/login-page';
 import { RegisterPage } from './register-page/register-page';
-
+import { Home } from './pages/home/home'
+ 
 export const routes: Routes = [
 
     { path: '', component: LandingPage },
     { path: 'login', component: LoginPage },
     { path: 'register', component: RegisterPage },
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: '' },
+    { path: 'home', component: Home}
 
 ];
