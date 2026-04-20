a05623afdbd9507ab7b15c53c625593b0608240e
Carl Foerster <cjfoerster5@gmail.com>
2026-04-20 12:46:21 -0400

    deleted CreateSchedulePage Component and make create schedule button a dropdown

--- ClassFrontend/src/app/pages/home/home.ts
+++ ClassFrontend/src/app/pages/home/home.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.sass',
 })
-export class Home {}
+
+export class Home {
+  showCreate = false;
+  scheduleName = '';
+
+  toggleCreate() {
+    this.showCreate = !this.showCreate;
+  }
+
+  createSchedule() {
+    console.log('New schedule:', this.scheduleName);
+  }
+}
